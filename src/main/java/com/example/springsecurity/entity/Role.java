46338ac4a216cd00eadb40ef66d6100f55efaf00
lucky2588 <duwcsthang2001@gmail.com>
2023-08-13 17:41:00 +0700

    Create Role.java

---
+++ src/main/java/com/example/springsecurity/entity/Role.java
@@ -0,0 +1,23 @@
+package com.example.springsecurity.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
