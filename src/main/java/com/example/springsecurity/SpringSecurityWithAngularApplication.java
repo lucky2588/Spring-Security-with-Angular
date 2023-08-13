406e44800872ef26c24ba3d2316226f9b4d73be7
lucky2588 <duwcsthang2001@gmail.com>
2023-08-13 17:40:28 +0700

    Create SpringSecurityWithAngularApplication.java

---
+++ src/main/java/com/example/springsecurity/SpringSecurityWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWithAngularApplication.class, args);
+    }
+
+}
