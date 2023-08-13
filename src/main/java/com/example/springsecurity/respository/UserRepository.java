ab17dec7ed0c65d33ab557d3a9a6581840d59a0f
lucky2588 <duwcsthang2001@gmail.com>
2023-08-13 17:40:44 +0700

    Create UserRepository.java

---
+++ src/main/java/com/example/springsecurity/respository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.respository;
+
+import com.example.springsecurity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+
+
+
+}
